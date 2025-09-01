e3f53446bbed372e9157b3c18faff00023316c29
IamsadVN <112055678+IamsadVN@users.noreply.github.com>
2025-09-01 09:17:51 +0700

    refactor: time to make an API

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,5 +1,30 @@
-import { systemLogger } from "./libs/logger.js";
+import Fastify from "fastify";
 
-systemLogger.info("Hello world");
-systemLogger.warn("Hello world");
-systemLogger.error("Hello world");
+import configEnv from "./config.js";
+import { apiLogger, systemLogger } from "./libs/logger.js";
+
+import useRoutes from "./routes/index.js";
+import useMiddlewares from "./middlewares/index.js";
+
+const fastify = Fastify();
+
+useRoutes(fastify);
+useMiddlewares(fastify);
+
+fastify.setErrorHandler((error, request, reply) => {
+	apiLogger.error(error);
+
+	reply.status(500).send("Internal Server Error");
+});
+
+try {
+	await fastify.listen({
+		host: "0.0.0.0",
+		port: configEnv.apiPort
+	});
+
+	apiLogger.info(`Server is listening at ${configEnv.apiPort}`);
+}
+catch (err) {
+	systemLogger.error(err);
+}
